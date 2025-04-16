ff565a0c1493977b374d8ae75d6015c9caab09ae
whoami <schmusi@systemli.org>
2025-04-16 13:11:01 +0200

    added random users by adding write_user_data function to users.py - now automatically adds user on registering.
    IMPORTANT: Should be checked again!

--- config/users.py
+++ config/users.py
@@ -1,5 +1,4 @@
 from base64 import b64encode
-from getpass import getpass
 import hashlib
 from logging import exception
 from config.yaml import load_yaml, dump_yaml
@@ -13,6 +12,7 @@ class User:
         self.id = id
         self.username = username
         self.password = password
+        self.authenticated = False
         
 def load_user_data():
     try:
@@ -26,12 +26,16 @@ def get_users(user_data:dict, users:list[User] = []) -> list[User]:
         user = User(count, user_yml['username'], user_yml['password'])
         users.append(user)
     global user_count; user_count = count
-    print(user_count)
     return users
 
-#def write_user_data():
-  #  dump_yaml(user_data, open_file(users_file, 'w'))
-    # User Data needs to be reloaded for changes to apply
+def write_user_data(users:list[User], user_data:dict = {}):
+    user_list = []
+    for user in users:
+        user_yml = {'id': user.id, 'username': user.username, 'password': user.password}
+        user_list.append(user_yml)
+    user_data = {'users': user_list}
+    with open(users_file, 'w') as file: 
+        dump_yaml(user_data, file)
 
 def hash_password(password:str):
     encoded=(password).encode()
--- ui/auth.py
+++ ui/auth.py
@@ -5,64 +5,61 @@ from fastapi.responses import RedirectResponse
 from starlette.middleware.base import BaseHTTPMiddleware
 from nicegui import Client, app, ui
 from config.config import theme
-from config.users import get_users, hash_password, load_user_data, User
-
-
+from config.users import get_users, load_user_data, User, write_user_data
+from config import routers
 
 class AuthMiddleware(BaseHTTPMiddleware):
-    """This middleware restricts access to all NiceGUI pages except login and / (index).
-    It redirects the user to the login page if they are not authenticated.
+    """This middleware restricts access to all NiceGUI pages except try_login and / (index).
+    It redirects the user to the try_login page if they are not authenticated.
     """
     
     async def dispatch(self, request: Request, call_next) -> Optional[RedirectResponse]:
-        # if user is not authenticated redirect them to login page
-        user_authenticated = app.storage.user.get('authenticated', False)
-        user_username = app.storage.user.get('username')
+        # if user is not authenticated redirect them to try_login page
+        user_data = load_user_data()
+        users = get_users(user_data)
         path = request.url.path
         if path in Client.page_routes.values():
             debug(path)
-            if not user_authenticated:
-                debug('User not authenticated')
-                if path not in {'/login', '/'}:
-                    debug('Redirecting to /login')
-                    return RedirectResponse('/login') # go to login page
-            else:
-                debug('User is authenticated')
-                # if a user tries to go to a page other than their own and is not admin => redirect them to their own page 
-                if path != f"/{user_username}":
-                    debug('User tries to go to page other than their own')
-                    if user_username != "admin":
-                        debug('User is not "admin"')
-                        return RedirectResponse(f"/{user_username}") # redirect to the users page
+            if path != "/":    
+                for user in users:
+                    if f"{routers.user.prefix}/{user.username}" == path:
+                        if user.authenticated == True:
+                            debug('User is authenticated')
+                        else:
+                            debug('User is not authenticated')
+                            return RedirectResponse("/")
+                    else: # if a user tries to go to a page other than their own and is not admin => redirect them to their own page 
+                        debug('User tries to go to page other than their own')
+                        return RedirectResponse(f"{routers.user.prefix}/{user.username}") # redirect to the users page
         return await call_next(request)
 
 app.add_middleware(AuthMiddleware)
 
-@ui.page('/login')
-def login():
-    theme.set_colors(), ui.dark_mode(theme.dark, on_change=lambda e: theme.toggle_dark(e.value))
+def try_login(username: str, password: str):
     user_data = load_user_data()
     users = get_users(user_data)
-    def try_login() -> None:  # local function to avoid passing username and password as arguments
-        if users is not None:
-            for user in users: # iterate through users to check if input matches a user
-                if user.username == username.value and user.password == hash_password(password.value):
-                    app.storage.user.update({'username': username.value, 'authenticated': True})
-                    ui.navigate.to(f"/{username.value}")  # go to the users page 
-                else:
-                    user = User(id=users.count(User)+1, username=username.value, password=hash_password(password.value))
-                    users.append(user)
-                    # TODO write to users.yml
-                    print(users)
-    with ui.card().classes('absolute-center'):
-        username = ui.input('Username').on('keydown.enter', try_login)
-        password = ui.input('Password', password=True, password_toggle_button=True).on('keydown.enter', try_login)
-        ui.button('Log in/Register', on_click=try_login)
+    if users is not None:
+        for user in users: 
+            # iterate through users to check if input matches a user
+            if user.username == username and user.password == password:
+                user.authenticated == True
+                ui.navigate.to(f"{routers.user.prefix}/{username}")  # go to the users page
+            else:
+                ui.notify("This username and password does not match any registered user.", color='negative')
+                return False
+            
+def register(username:str, password:str):
+        user_data = load_user_data()
+        users = get_users(user_data)
+        user = User(id=users.count(User)+1, username=username, password=password)
+        users.append(user); write_user_data(users)
+        ui.navigate.to(f"/{routers.user.prefix}/{username}")
     
 def check_logout():
     if app.storage.user.get('authenticated', False):
         def try_logout():
             app.storage.user.update({'authenticated': False})
+            ui.navigate.to("/")
         with ui.button(on_click=try_logout()):
             ui.tab(name='logout', label='Logout', icon='logout')
             
