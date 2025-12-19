aab8f06de18e041a4360c9f22ec970101abf0668
whoami <aberallesisgut@riseup.net>
2025-12-19 18:16:36 +0100

    Bugfix: If there is no data found for user id it notifies instead of crashing

--- src/inventurgui/helper/magic_link.py
+++ src/inventurgui/helper/magic_link.py
@@ -1,6 +1,6 @@
 import os
 
-from nicegui import json, app
+from nicegui import json, app, ui
 
 from inventurgui.helper.config import get_path, config
 from inventurgui.helper.safe_url import url_safe
@@ -9,6 +9,11 @@ def get_magic_link() -> str:
     return f"https://{config['domain']}/{url_safe(config['cart']['label'])}?id={app.storage.browser['id']}"
 
 def load_data_from_magic_link(old_id:str, new_id:str) -> None:
-    with open(get_path(f'/users/storage-user-{new_id}.json'), 'r') as f:
-        app.storage.user.update(json.loads(f.read()))
-        app.storage.browser.update({'id': new_id})
+    try:
+        with open(get_path(f'/users/storage-user-{new_id}.json'), 'r') as f:
+            app.storage.user.update(json.loads(f.read()))
+            app.storage.browser.update({'id': new_id})
+            app.storage.user.initialize()
+    except FileNotFoundError:
+        ui.notify(f"Sorry, couldn't find data for =id?{new_id}.", type='negative', position='center', text='secondary')
+        ui.timer(5, lambda: ui.navigate.to("/"), once=True)
