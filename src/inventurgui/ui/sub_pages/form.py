50ee3b13b4da9d26d6c081ab16a9a1c4615e041b
whoami <schmusi@systemli.org>
2026-01-12 17:13:11 +0100

    Form now uses a class instead of a huge function with sub_functions.

--- src/inventurgui/ui/sub_pages/form.py
+++ src/inventurgui/ui/sub_pages/form.py
@@ -3,7 +3,11 @@ import re
 from contextlib import suppress
 
 from nicegui import app, ui, PageArguments
+from nicegui.elements.checkbox import Checkbox
+from nicegui.elements.date import Date
 from nicegui.elements.drawer import LeftDrawer
+from nicegui.elements.editor import Editor
+from nicegui.elements.input import Input
 from nicegui.observables import ObservableDict
 
 from inventurgui.helper.config import config, EMAIL_REGEX, load_config, get_path
@@ -45,113 +49,126 @@ async def form_page(ld: LeftDrawer, warehouses: list[Warehouse], args: PageArgum
     with form_panels:
         with ui.tab_panel("default").classes("xl:w-350 mx-auto p-5"):
             with ui.grid(columns=2) as grid:
-                create_form(warehouses)
+                Form().create(warehouses)
                 if terms.get("display"):
                     await render_markdown(form.get("terms"))
         with ui.tab_panel(form.get("tab_label")).classes("m-0"):
-            create_form(warehouses)
+            Form().create(warehouses)
         if terms.get("display"):
             with ui.tab_panel(terms.get("label")).classes("m-0 p-0"):
                 await render_markdown(form.get("terms"))
     set_panel()
     ui.on("resize", lambda: set_panel(), throttle=0.8, trailing_events=True)
 
+class Form:
+	def __init__(self):
+		self.valid = False
+		self.inputs: list[Input|Editor|Checkbox] = []
+		self.config: dict[str, str | dict[str, str]] = load_config()["form"]
+		self.dates: Date = ui.date()
+		self.request: ObservableDict = app.storage.user.get("form")
 
-def create_form(warehouses: list[Warehouse]):
-    form: dict[str, str | dict[str, str]] = load_config()["form"]
+	def validate(self) -> None:
+		self.valid = False
+		with suppress(NameError):
+			if any([not i.value for i in self.inputs]) or self.dates.value is None:
+				self.valid = False
+			else:
+				self.valid = True
 
-    def validate_form() -> bool:
-        rules = [dates.value]
-        [rules.append(i.value) for i in inputs]
-        with suppress(NameError):
-            for check in rules:
-                if not check:
-                    return False
-        return True
+	async def submit(self, warehouses) -> None:
+		is_update = True if self.request.get("sent") else False
+		self.request.update({"finish": config.get("processing")})
+		ui.navigate.to(f"/{url_safe(config['finish']['label'])}")
+		self.request.update(
+			{"sent": datetime.date.today().strftime(config["date_format"])}
+			if not self.request.get("sent")
+			else {"updated": datetime.date.today().strftime(config["date_format"])}
+		)
+		try:
+			send_mail(self.request, warehouses, type="update" if is_update else "request")
+			await save_request(self.request, warehouses)
+			filename = get_path(f"lists/{config['finish'].get('filename')}-{self.request.get('name')}.ods")
+			await write_download_list(filename, warehouses)
+			self.request.update({"download": str(filename)})
+			self.request.update({"message": None})
+			self.request.update({"finish": self.config.get("success")})
+		except Exception as exception:
+			self.request.update({"finish": self.config.get("failure")})
+			send_mail(self.request, warehouses, type="failure", exception=exception)
 
-    async def handle_submit():
-        is_update = True if request.get("sent") else False
-        request.update({"finish": form.get("processing")})
-        ui.navigate.to(f"/{url_safe(config['finish']['label'])}")
-        request.update(
-            {"sent": today.strftime(config["date_format"])}
-            if not request.get("sent")
-            else {"updated": today.strftime(config["date_format"])}
-        )
-        try:
-            send_mail(request, warehouses, type="update" if is_update else "request")
-            await save_request(request, warehouses)
-            filename = get_path(f"lists/{form.get('filename')}-{request.get('name')}.ods")
-            await write_download_list(filename, warehouses)
-            request.update({"download": str(filename)})
-            request.update({"message": None})
-            request.update({"finish": form.get("success")})
-        except Exception as exception:
-            request.update({"finish": form.get("failure")})
-            send_mail(request, warehouses, type="failure", exception=exception)
+	async def delete_request(self, warehouses:list[Warehouse]) -> None:
+		ui.notify("Deleting...")
+		self.request.update({"finish": self.config.get("processing")})
+		ui.navigate.to(f"/{url_safe(config['finish']['label'])}")
+		send_mail(self.request, warehouses, type="delete")
+		await delete_request(self.request)
+		ui.notify("Deleted")
+		self.request.update({"deleted": True, "finish": self.config.get("deleted")})
+		#TODO maybe reset app.storage.user
 
-    async def handle_delete():
-        ui.notify("Deleting...")
-        request.update({"finish": form.get("processing")})
-        ui.navigate.to(f"/{url_safe(config['finish']['label'])}")
-        send_mail(request, warehouses, type="delete")
-        await delete_request(request)
-        ui.notify("Deleted")
-        request.update({"deleted": True, "finish": form.get("deleted")})
+	def create(self, warehouses:list[Warehouse]):
+		with ui.dialog() as delete_dialog:
+			with ui.card():
+				ui.label("Bist du sicher?".upper()).classes("w-full pt-2 text-center tracking-widest")
+				delete = ui.button(self.config.get("delete"), icon=self.config.get("delete_icon"), color="negative")
+				delete.on_click(lambda: self.delete_request(warehouses))
 
-    today: datetime.date = datetime.date.today()
-    request: ObservableDict = app.storage.user.get("form")
-    with ui.dialog() as delete_dialog:
-        with ui.card():
-            ui.label("Bist du sicher?".upper()).classes("w-full pt-2 text-center tracking-widest")
-            ui.button(form.get("delete"), icon=form.get("delete_icon"), color="negative", on_click=handle_delete)
-    with ui.grid(columns=2).classes("w-full bg-dark pb-10 h-screen flex-column") as grid:
-        with ui.column().classes("ml-auto pb-20") as submit_column:
-            submit = ui.button(form.get("send"), icon=form.get("send_icon"), on_click=handle_submit)
-            submit.props("text-color=secondary rounded")
-            submit.classes("p-3 sm:w-80 text-lg")
-            delete = ui.button(form.get("delete"), icon=form.get("delete_icon"), on_click=lambda: delete_dialog.open())
-            delete.bind_visibility_from(request, "sent")
-            delete.props("text-color=gray-300 rounded").classes("p-3 bg-negative sm:w-80 text-lg")
+		with ui.grid(columns=2).classes("w-full bg-dark pb-10 h-screen flex-column") as grid:
+			with ui.column().classes("ml-auto pb-20") as submit_column:
+				submit = ui.button(self.config.get("send"), icon=self.config.get("send_icon"))
+				submit.on_click(lambda: self.submit(warehouses))
+				submit.props("text-color=secondary rounded")
+				submit.classes("p-3 sm:w-80 text-lg")
+				delete = ui.button(self.config.get("delete"), icon=self.config.get("delete_icon"),
+				                   on_click=lambda: delete_dialog.open())
+				delete.bind_visibility_from(self.request, "sent")
+				delete.props("text-color=gray-300 rounded").classes("p-3 bg-negative sm:w-80 text-lg")
 
-        with ui.column().classes("mx-auto max-sm:col-span-2"):
-            dates_label = ui.label(f"{form.get('start')} - {form.get('end')}".upper()).classes(
-                "w-full pt-2 text-center tracking-widest"
-            )
-            dates = ui.date().classes("p-0").props("range minimal flat")
-            dates.props[":options"] = f'date => date >= "{today:%Y/%m/%d}"'
-            dates.bind_value(request, "dates")
-            dates.on_value_change(lambda: submit.enable() if value else submit.disable())
+			with ui.column().classes("mx-auto max-sm:col-span-2") as column:
+				dates_label = ui.label(f"{self.config.get('start')} - {self.config.get('end')}".upper()).classes(
+					"w-full pt-2 text-center tracking-widest"
+				)
+				dates = self.dates.classes("p-0").props("range minimal flat")
+				dates.move(column)
+				dates.props[":options"] = f'date => date >= "{datetime.date.today():%Y/%m/%d}"'
+				dates.bind_value(self.request, "dates")
+				dates.on_value_change(lambda: self.validate())
 
-        inputs = []
-        email_validation = {form.get("email_invalid"): lambda v: True if re.match(EMAIL_REGEX, v) else False}
-        input_validation = {form.get("please_fill"): lambda v: len(v) > 0}
-        with ui.column().classes("items-stretch max-sm:col-span-2"):
-            for key, value in form.get("input").items():
-                if not key == "message":
-                    i = ui.input(value, validation=email_validation if key == "email" else input_validation)
-                else:
-                    i = ui.editor(
-                        placeholder=form.get("message") if not request.get("sent") else form.get("update_message")
-                    )
-                    i.classes("col-span-2")
-                    i.move(grid)
-                if key == "name" or key == "email":
-                    i.bind_enabled_from(request, "sent", backward=lambda v: not v)
-                i.on_value_change(lambda s=submit: s.enable() if validate_form() else s.disable())
-                i.bind_value(request, key).props("debounce=1000")
-                inputs.append(i)
+			email_validation = {self.config.get("email_invalid"): lambda v: True if re.match(EMAIL_REGEX, v) else False}
+			input_validation = {self.config.get("please_fill"): lambda v: len(v) > 0}
+			with ui.column().classes("items-stretch max-sm:col-span-2"):
+				for key, value in self.config.get("input").items():
+					if not key == "message": # Negative if statement because editor has to be moved
+						i = ui.input(value, validation=email_validation if key == "email" else input_validation)
+						i.without_auto_validation()
+						i.on('blur', lambda x=i: x.validate())
+					else:
+						i = ui.editor(value='',
+							placeholder=self.config.get("message") if not self.request.get("sent")
+							else self.config.get("update_message")
+						)
+						i.classes("col-span-2")
+						i.move(grid) # Moves editor
+					if key == "name" or key == "email":
+						i.bind_enabled_from(self.request, "sent", backward=lambda v: not v)
+					i.on_value_change(lambda: self.validate())
+					i.bind_value(self.request, key)
+					self.inputs.append(i)
 
-        with ui.column().classes("pb-20"):
-            for value in form.get("checkbox").values():
-                c = ui.checkbox(value)
-                c.on_value_change(lambda s=submit: s.enable() if validate_form() else s.disable())
-                inputs.append(c)
+			with ui.column().classes("pb-20"):
+				for value in self.config.get("checkbox").values():
+					c = ui.checkbox(value)
+					c.on_value_change(lambda: self.validate())
+					self.inputs.append(c)
 
-        # Move so it is available as variable above
-        submit_column.move(grid)
-        submit.disable()
-        submit.bind_text_from(request, "sent", backward=lambda v: form.get("update") if v else form.get("send"))
-        submit.bind_icon_from(
-            request, "sent", backward=lambda v: form.get("update_icon") if v else form.get("send_icon")
-        )
+			# Move so it is available as variable above
+			submit_column.move(grid)
+			submit.bind_enabled_from(self, 'valid')
+			submit.bind_text_from(
+				self.request, "sent",
+				backward=lambda v: self.config.get("update") if v else self.config.get("send"))
+			submit.bind_icon_from(
+				self.request, "sent",
+				backward=lambda v: self.config.get("update_icon") if v else self.config.get("send_icon")
+			)
