5181b777d16e507c094e1471821398cfc1bc519c
whoami <aberallesisgut@riseup.net>
2026-01-29 15:12:20 +0100

    Remove old code

--- src/inventurgui/ui/markdown.py
+++ src/inventurgui/ui/markdown.py
@@ -14,26 +14,12 @@ async def render_markdown(values: dict[str, str]) -> Markdown:
             LOGGER.debug(f"Creating {label} with the content of {path}...")
             text = f.read()
     except FileNotFoundError:
-        if url := values.get("url"):
-            LOGGER.debug(f"Getting {label} content from {url}...")
-            try:
-                response = await run.io_bound(lambda: httpx.get(url, timeout=2))
-                print(response)
-                with open(path, "w") as f:
-                    f.write(response.text)
-                LOGGER.info(f"Successfully fetched {label} content from {url}!")
-            except httpx.ReadTimeout:
-                text = (
-                    f"<br>The URL: [{url}]({url}) for page: **{label}** could not be fetched."
-                    f"<br><br>Please check your Internet Connection, the url and try opening it manually."
-                )
-        else:
-            text = (
-                f"<br>path:{path.name}<br> in config for page **{label}** does not exist."
-                f"<br>Please review your config, read the logs and check /files."
-                f"<br>If you started the program for the first time i may have fetched the page by now."
-                f"<br>In that case a page reload might also fix the problem."
-            )
+        text = (
+            f"<br>path:{path.name}<br> in config for page **{label}** does not exist."
+            f"<br>Please review your config, read the logs and check /files."
+            f"<br>If you started the program for the first time i may have fetched the page by now."
+            f"<br>In that case a page reload might also fix the problem."
+        )
     return ui.markdown(text).classes(
         "p-10 pt-5 mx-auto text-justify hyphens-none sm:text-base/6 sm:antialiasing text-gray-300 max-w-180"
     )
