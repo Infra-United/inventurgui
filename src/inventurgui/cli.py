55ff3a360a19a41415e0b6a10b2287b21f8d0d05
whoami <aberallesisgut@riseup.net>
2026-02-06 12:14:52 +0100

    CLI Cleanup

--- src/inventurgui/cli.py
+++ src/inventurgui/cli.py
@@ -15,20 +15,13 @@ def get_args() -> Namespace:
     # Get argparser
     argparser = argparse.ArgumentParser(
         prog="inventurgui",
-        description="A web application to display the content of an .ods (Libre Office Calc) file in a nice way.",
+        description="A web app to display the content of an .ods (Libre Office Calc) file in a nice way.",
     )
     # Add arguments to argparser
-    argparser.add_argument(
-        "-c", "--helper", dest="config_file", help="specify path to helper file, defaults to files/helper.yml"
-    )
-    argparser.add_argument("-r", "--reload", dest="reload", help="enable reloader", action="store_true"
-    )
-    argparser.add_argument(
-        "-d", "--dev", dest="debug", help="set the log level to debug", action="store_true"
-    )
-    # argparser.add_argument("-p", "--set-password", dest='set_pass', help='set the password required for the admin panel (configuration)', action='store_true')
-    # argparser.add_argument("-u", "--users", dest='users_file', help='specify path to users_file, defaults to files/users.yml')
-    argparser.set_defaults(config_file="config.yml", log_level="info", users_file="../../files/users.yml")
+    argparser.add_argument("-c", dest="config_file", help="specify path to config, defaults to config.yml")
+    argparser.add_argument("-r", "--reload", dest="reload", help="enable reloader", action="store_true")
+    argparser.add_argument( "-d", "--dev", dest="debug", help="set the log level to debug", action="store_true")
+    argparser.set_defaults(config_file="config.yml")
     # Parse args to dictionary
     args: Namespace = argparser.parse_args()
     if args.debug:
