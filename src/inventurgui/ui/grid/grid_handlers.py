9c635eb3a317654598ac68b930b60849ad210c00
whoami <aberallesisgut@riseup.net>
2026-03-14 21:07:51 +0100

    Bugfix: no weight caused type error on editing amount

--- src/inventurgui/ui/grid/grid_handlers.py
+++ src/inventurgui/ui/grid/grid_handlers.py
@@ -37,7 +37,8 @@ async def update_amount(grid: AgGrid, warehouse:Warehouse, event: GenericEventAr
         return
     Cache.amounts(warehouse.name).update({row_id: int(new_value)})
     data.update({event.args['colId']: new_value})
-    data[settings.columns['total_weight']] = int(new_value) * data[settings.columns['weight']]
+    with suppress(TypeError):
+        data[settings.columns['total_weight']] = int(new_value) * data[settings.columns['weight']]
     await grid.run_row_method(row_id, "setData", data)
     #grid.options.get("pinnedBottomRowData")[0].update({settings.columns["total_weight"]: await warehouse.total_weight()})
 
