74f26e7fbb7b3630c8c055b990ce9f70e2ed4916
whoami <aberallesisgut@riseup.net>
2026-03-17 17:11:51 +0100

    Added help.py (wiki_pages) (forgot)

---
+++ src/inventurgui/ui/sub_pages/help.py
@@ -0,0 +1,14 @@
+from nicegui import ui
+from nicegui.elements.drawer import RightDrawer, LeftDrawer
+
+from inventurgui.helper.logger import LOGGER
+
+
+def wiki_page(name:str, html:str, ld:LeftDrawer, rd:RightDrawer, style:str):
+    ld.hide()
+    ui.page_title(name)
+    LOGGER.debug(f"Creating wiki page {name}...")
+    with ui.tab_panel(name).classes("m-0 p-0 items-center w-full scroll h-dvh"):
+        html = ui.html(f"<head>{style}</head> {html}", sanitize=False)
+        html.classes("mx-auto items-center px-8 sm:px-20 text-base/6")
+        html.classes("hyphens-none sm:text-base/6 sm:antialiasing text-gray-300 max-w-180")
