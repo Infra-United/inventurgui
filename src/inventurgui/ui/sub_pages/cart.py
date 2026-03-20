2138bc9f09b68abbd899b13664d773e099aa3f22
whoami <schmusi@systemli.org>
2026-03-20 14:16:01 +0100

    Added back_fab to cart

--- src/inventurgui/ui/sub_pages/cart.py
+++ src/inventurgui/ui/sub_pages/cart.py
@@ -10,7 +10,7 @@ from inventurgui.io.cache import Cache
 from inventurgui.io.warehouse import Warehouse
 from inventurgui.ui.grid.grid import create_aggrid
 from inventurgui.ui.helper.magic_link import load_data_from_magic_link
-from inventurgui.ui.helper.reusable_elements import badge, next_fab, tabs, tab_panels
+from inventurgui.ui.helper.reusable_elements import badge, next_fab, tabs, tab_panels, back_fab
 
 
 async def cart_page(ld: LeftDrawer, warehouses: list[Warehouse], args: PageArguments) -> None:
@@ -52,3 +52,4 @@ async def cart_page(ld: LeftDrawer, warehouses: list[Warehouse], args: PageArgum
     truck_panels.set_value(warehouses[0].name.upper())
     LOGGER.info("Created cart page")
     next_fab(settings.form)
+    back_fab(settings.warehouse)
