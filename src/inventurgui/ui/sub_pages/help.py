0a1fa4f3e7c2e231f069f55aee6c4a9bc0e9a6ee
whoami <aberallesisgut@riseup.net>
2026-03-29 13:52:21 +0200

    Added help_page function to separate more clearly

--- src/inventurgui/ui/sub_pages/help.py
+++ src/inventurgui/ui/sub_pages/help.py
@@ -8,7 +8,14 @@ from inventurgui.io.wiki import WIKI_ROOT
 from inventurgui.ui.helper.markdown import render_markdown
 
 
-async def wiki_page(name: str, html: str, ld: LeftDrawer, md: str, style: str):
+async def help_page(ld: LeftDrawer, md: dict[str, str]):
+    ld.hide()
+    ui.page_title(f"{settings.help['label']}")
+    LOGGER.debug(f"Creating help page...")
+    with ui.tab_panel("help").classes("m-0 p-0 max-sm:pb-20 items-center w-full scroll h-dvh"):
+        render_markdown(md.get(settings.help["label"]))
+
+async def wiki_page(name: str, html: str, ld: LeftDrawer, style: str):
     ld.hide()
     ui.page_title(f"{WIKI_ROOT.capitalize()}/{name}")
     LOGGER.debug(f"Creating wiki page {name}...")
@@ -17,5 +24,3 @@ async def wiki_page(name: str, html: str, ld: LeftDrawer, md: str, style: str):
             html = ui.html(f"<head>{style}</head> {html}", sanitize=False)
             html.classes("mx-auto items-center px-8 sm:px-20 text-base/6")
             html.classes("hyphens-none sm:text-base/6 sm:antialiasing text-gray-300 max-w-180")
-        else:
-            render_markdown(md)
