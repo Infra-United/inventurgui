fc5266d1097fdfe618e8d7b2e46a419aec49d0cb
Noa <noa_filou+github@disroot.org>
2026-03-31 22:06:37 +0200

    Implemented Share Button for Wiki in browsers that don't implement it.

--- src/inventurgui/ui/sub_pages/help.py
+++ src/inventurgui/ui/sub_pages/help.py
@@ -1,8 +1,9 @@
-
+from bs4 import BeautifulSoup
 from nicegui import ui
 from nicegui.elements.drawer import LeftDrawer
 
 from inventurgui.helper.config import settings
+from inventurgui.helper.i18n import i18n
 from inventurgui.helper.logger import LOGGER
 from inventurgui.io.cache import Cache
 from inventurgui.ui.helper.markdown import render_markdown
@@ -20,13 +21,24 @@ async def wiki_page(name: str, content: str, ld: LeftDrawer, style: str):
     ui.page_title(f"{name.capitalize()}")
     LOGGER.debug(f"Creating wiki page {name}...")
 
-    def share_content():
-        ui.run_javascript("""
-        navigator.share({
-                    title: document.title,
-                    url: window.location.href,
-                }).catch(console.error);
-        """)
+    async def share_content() -> None:
+        if Cache.share():
+            await ui.run_javascript("""
+            navigator.share({
+                        title: document.title,
+                        url: window.location.href,
+                    }).catch(console.error);
+            """)
+        else:
+            url = await ui.run_javascript('window.location.href')
+            soup = BeautifulSoup(content, 'html.parser')
+            description = ""
+            for i in soup.find_all(['p', 'span' 'em']):
+                if desc := i.get_text(separator=" ", strip=True):
+                    description = desc.lstrip()[:400]
+                    break
+            ui.clipboard.write(f"{name.capitalize()}:\n\n{description} [...]\n\n{url}")
+            ui.notify(i18n.get("wiki.url_copied"), position='center', color='primary', textColor='secondary')
 
     with ui.tab_panel(name).classes("m-0 p-0 max-sm:pb-20 items-center w-full scroll h-dvh"):
         if settings.help["wiki"]:
@@ -38,6 +50,5 @@ async def wiki_page(name: str, content: str, ld: LeftDrawer, style: str):
                     if Cache.share() is None:
                         ui.add_head_html(
                             """<script> emitEvent('share_check', {'is_allowed': navigator.share}); </script>""")
-                        ui.on('share_check', lambda e: Cache.set_share(e.args['is_allowed']))
-                    if Cache.share():
-                        ui.fab(icon='share').on('click', share_content).props('active-icon=share')
+                        ui.on('share_check', lambda e: Cache.set_share(True if e.args['is_allowed'] == 'true' else False))
+                    ui.fab(icon='share').on('click', share_content).props('active-icon=share')
